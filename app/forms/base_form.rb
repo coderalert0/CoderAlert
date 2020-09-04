97e3a8a7d04fd5d89f7c5777ac02e63ba9247e96
Gurpreet Dhaliwal <coderalert0@gmail.com>
2020-09-04 16:21:13 -0400

    Added support for form object pattern

---
+++ app/forms/base_form.rb
@@ -0,0 +1,26 @@
+class BaseForm
+  include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+  # include EmailSending
+  # include ArgsKeyFirst
+
+  def submit
+    if valid?
+      _submit
+    else
+      _handle_invalid_form
+    end
+  end
+
+  def _submit
+    not_implemented
+  end
+
+  def _handle_invalid_form
+    false
+  end
+
+  def filter_blank(item)
+    item.blank? ? nil : item
+  end
+end
---
+++ app/forms/create_project_form.rb
@@ -0,0 +1,14 @@
+class CreateProjectForm < BaseForm
+  attr_accessor :name
+  attr_writer :project
+
+  nested_attributes :name, to: :project
+
+  accessible_attr :name
+
+  validates_presence_of :name
+
+  def project
+    @project ||= Project.new
+  end
+end
