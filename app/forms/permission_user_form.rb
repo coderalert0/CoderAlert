2c75fc032194ebc6e3c5b21106e5c95e4a7edcfc
Gurpreet Dhaliwal <coderalert0@gmail.com>
2020-10-10 21:47:19 -0400

    Creating and destroying AlertSettings when a user is permissioned/unpermissioned to a project

--- app/forms/permission_user_form.rb
+++ app/forms/permission_user_form.rb
@@ -1,12 +1,15 @@
 class PermissionUserForm < BaseForm
-  attr_accessor :user, :project
+  attr_accessor :user_id, :project
 
-  accessible_attr :user
+  accessible_attr :user_id
 
   def _submit
-    user = User.find(@user)
-    project_user = project.project_users.build(user: user)
-    project_user.save!
+    ActiveRecord::Base.transaction do
+      project_user = project.project_users.build(user: user)
+      project_user.save!
+      create_slack_alert_settings if project.slack_authorization.present?
+      create_contact_alert_settings
+    end
   end
 
   private
@@ -14,5 +17,19 @@ class PermissionUserForm < BaseForm
   def initialize(args = {})
     self.user = User.find(args[:user]) if args[:user]
     super
+  end
+
+  def create_slack_alert_settings
+    AlertSetting.create(alertable: project.slack_authorization, user: user, project: project, alert: true)
+  end
+
+  def create_contact_alert_settings
+    user.contacts.each do |contact|
+      AlertSetting.create(alertable: contact, user: user, project: project, alert: false)
+    end
+  end
+
+  def user
+    User.find(user_id)
   end
 end
--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -2,5 +2,16 @@ class ProjectUser < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
+  has_many :alert_settings, through: :project
+
+  after_destroy :destroy_alert_settings
+
   validates_presence_of :project, :user
+
+  private
+
+  # workaround for activerecord error for dependent destroy
+  def destroy_alert_settings
+    alert_settings.each(&:destroy)
+  end
 end
