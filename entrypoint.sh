7aca544b7cdee0080b4fd0502b4a11fbad5ccfb9
hieu <hieu.tnbk2011@yahoo.com>
2020-11-11 16:54:41 +0000

    add master.key

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ rm -f /coder_alert/tmp/pids/server.pid
 # Wait for database running:
 sleep 5
 while [[ $(nc -z $DATABASE_HOST 5432 &> /dev/null; echo $?) -ne 0 ]]; do echo pod is not running;sleep 3; done
+echo $MASTER_KEY > /code_alert/tmp/config/master.key
 # Migrate the database before running:
 rake db:migrate
 # RUN SUPERVISOR for process
