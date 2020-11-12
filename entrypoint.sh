539d9934792d9209a1db58df888ead57feb8d934
hieu <hieu.tnbk2011@yahoo.com>
2020-11-12 21:23:34 +0000

    Add precompile

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ rm -f /coder_alert/tmp/pids/server.pid
 # Wait for database running:
 sleep 5
 while [[ $(nc -z $DATABASE_HOST 5432 &> /dev/null; echo $?) -ne 0 ]]; do echo pod is not running;sleep 3; done
-echo $MASTER_KEY > /coder_alert/config/master.key
+# compile static files
+rake assets:precompile
 # Migrate the database before running:
 rake db:migrate
 # RUN SUPERVISOR for process
