bbd11bb4f721185c867664f2aadf1b66de9ff286
Gurpreet Dhaliwal <coderalert0@gmail.com>
2020-11-12 00:33:07 -0500

    Updated environment configurations to point to correct host for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "coder_alert_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'staging.coderalert.com' }
+  config.action_mailer.default_url_options = { host: 'app.coderalert.com' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
