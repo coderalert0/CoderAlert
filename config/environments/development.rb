295c45db4540ab61b3db108a65cbf89b28d9f467
Gurpreet Dhaliwal <coderalert0@gmail.com>
2020-11-13 06:26:52 -0500

    Adding action_mailer config to application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,23 +30,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  config.action_mailer.perform_deliveries = true
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      port: 587,
-      address: 'email-smtp.us-east-2.amazonaws.com',
-      user_name: Rails.application.credentials.dig(:aws, :ses_username),
-      password: Rails.application.credentials.dig(:aws, :ses_password),
-      authentication: :login,
-      enable_starttls_auto: true
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
