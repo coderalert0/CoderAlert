217197f24aabf554ad26c04c72f48ad716e8e834
Gurpreet Dhaliwal <coderalert0@gmail.com>
2020-11-15 02:44:24 -0500

    Add conditional check on environment for adding ssh key in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
-# Create SSH folder
-USER_SSH_KEYS_FOLDER=~/.ssh
-[ ! -d "$USER_SSH_KEYS_FOLDER" ] && mkdir -p $USER_SSH_KEYS_FOLDER
-env >> /etc/environment
-# Add SSH_KEY
-echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
+
+if [ $RAILS_ENV  == "production" ]; then
+  # Create SSH folder
+  USER_SSH_KEYS_FOLDER=~/.ssh
+  [ ! -d "$USER_SSH_KEYS_FOLDER" ] && mkdir -p $USER_SSH_KEYS_FOLDER
+  env >> /etc/environment
+
+  # Add SSH_KEY
+  echo $SSH_PUBLIC_KEY > ~/.ssh/authorized_keys
+fi
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /coder_alert/tmp/pids/server.pid
+
 # Wait for database running:
 sleep 5
 while [[ $(nc -z $DATABASE_HOST 5432 &> /dev/null; echo $?) -ne 0 ]]; do echo pod is not running;sleep 3; done
+
 # Migrate the database before running:
 rake db:migrate
 
